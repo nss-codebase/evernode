ea771f0098c79dc62ba43a2dfcef1fd456aea723
Chyld Medford <chyld.medford@gmail.com>
2014-12-18 11:51:44 -0600

    auth

---
+++ test/e2e/authentication/login.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var cp = require('child_process'),
+    h  = require('../../helpers/helpers'),
+    db = h.getdb();
+
+describe('login', function(){
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../../scripts/clean-db.sh', [db], {cwd:__dirname + '/../../scripts'}, function(err, stdout, stderr){
+      browser.get('/#/login');
+      done();
+    });
+  });
+
+  it('should get login page', function(){
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('login');
+  });
+
+  it('should login user', function(){
+    element(by.model('user.username')).sendKeys('bob');
+    element(by.model('user.password')).sendKeys('123');
+    element(by.css('button[ng-click]')).click();
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('home');
+  });
+
+  it('should not login user', function(){
+    element(by.model('user.username')).sendKeys('bob');
+    element(by.model('user.password')).sendKeys('1234');
+    element(by.css('button[ng-click]')).click();
+    expect(element(by.css('div[ui-view] > h1')).getText()).toEqual('login');
+  });
+});
