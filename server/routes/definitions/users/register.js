2457cad9aaae605f9d174c799ddff93bce2029b3
Chyld Medford <chyld.medford@gmail.com>
2014-12-21 11:40:54 -0600

    node v0.10.33 fixes cert error

--- server/routes/definitions/users/register.js
+++ server/routes/definitions/users/register.js
@@ -16,8 +16,6 @@ module.exports = {
   auth: false,
   handler: function(request, reply){
     User.register(request.payload, function(err){
-      console.log('REGISTER');
-      console.log(err);
       reply().code(err ? 400 : 200);
     });
   }
